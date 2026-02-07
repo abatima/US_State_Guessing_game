556c23ceda39e0704db728f8e20f54b2d4d4087b
abatima <renat.kulalov.work@gmail.com>
2026-02-07 10:44:01 +0100

    added exit and saving missing states to a file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
-written_state = turtle.Turtle()
 screen.title("US States Game")
 background_image = "blank_states_img.gif"
 correct_counter = 0
@@ -12,23 +11,27 @@ screen.addshape(background_image)
 turtle.shape(background_image)
 
 data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 
-while game_is_on:
+while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{correct_counter}/50 Guess the state", prompt="What's the name?").title()
 
-    for state in data.state:
-        if state == answer_state and answer_state not in guessed_states:
-            guessed_states.append(answer_state)
-            correct_counter += 1
+    if answer_state == "exit".title():
+        missing_states = list(set(all_states) - set(guessed_states))
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        print(missing_states)
+        break
 
-            #drawing
+    if answer_state in data.state.values and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        correct_counter += 1
 
-            state_row = data[data.state == answer_state]
-            written_state.teleport(state_row.x.item(), state_row.y.item())
-            written_state.hideturtle()
-            written_state.write(answer_state)
-
-
-
-    print(answer_state)
-turtle.mainloop()
+        #drawing
+        guessed_state_row = data[data.state == answer_state]
+        x_cor = guessed_state_row.x.item()
+        y_cor = guessed_state_row.y.item()
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.teleport(x_cor, y_cor)
+        t.write(answer_state)
